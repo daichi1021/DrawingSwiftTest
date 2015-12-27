57bf4df0de39032ee80cab9688efbfb6f67ca996
Daichi Mizoguchi <dmizoguchi@terrasky.co.jp>
2015-12-27 17:17:09 +0900

    Initial Commit

---
+++ DrawingTestSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrawingTestSwift
+//
+//  Created by Daichi Mizoguchi on 2015/12/27.
+//  Copyright © 2015年 Daichi Mizoguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
